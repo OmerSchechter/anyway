1627e86032e7053c1e915025eed212511bb83101
Omer Schechter <omers.iphone@gmail.com>
2015-04-01 17:48:11 +0300

    Removed more 'bind(this)'.

--- static/js/app.js
+++ static/js/app.js
@@ -302,7 +302,7 @@ $(function() {
 
             _.each(this.oms.markersNearAnyOtherMarker(), function(marker){
                 marker.icon = MULTIPLE_ICONS[groupsSeverities[marker.view.model.get("groupID") -1]];
-            }.bind(this));
+            });
         },
         downloadCsv: function() {
             if (this.markers.length > 0) {
